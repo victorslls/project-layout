10757201097d1106259f65fdb6603c3ed2c43562
victor slls <victorslls.com>
2023-11-10 12:44:46 -0300

    ajuste no z-index

--- components/Circles.js
+++ components/Circles.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 const Circles = () => {
   return (
     <div className="2-[200px] xl:w-[300px] absolute -right-16 -bottom-2 
-    mix-blend-color-dodge animate-pulse duration-75 z-10">
+    mix-blend-color-dodge animate-pulse duration-75 z-5">
     <Image
       src={'/circles.png'} 
       width={260}
