78c79f053a61ed876160a2e0ce62c15314a13cef
victor slls <victorslls.com>
2023-11-10 12:25:09 -0300

    problema resolvido

--- components/Socials.js
+++ components/Socials.js
@@ -7,7 +7,7 @@ import {RiYoutubeLine, RiInstagramLine, RiFacebookLine,RiDribbbleLine,RiBehanceL
 
 const Socials = () => {
   return(  
-    <div className="flex items-center gap-x-5 text-lg">
+    <div className="flex items-center gap-x-5 text-lg ">
       <Link href={''} className="hover:text-accent transition-all duration-300">
         <RiYoutubeLine/>
       </Link>
